760fc06aafca8173f1e440062216f87488f33454
tonkoshkur <i.tonkoshkur97@gmail.com>
2019-10-05 02:12:04 +0300

    new project

---
+++ PackageAssistantApp/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void QuickInstallation_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackupButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RecoveryButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ContactTransferButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DataTransferButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateGAccButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DiagnosticsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonPackage1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonPackage2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonPackage3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonPackage4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonPackage5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonPackage6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
