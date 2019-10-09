6856dbd538ec660fed5798d8765091783ba59425
tonkoshkur <i.tonkoshkur97@gmail.com>
2019-10-09 17:25:36 +0300

    This folder

---
+++ PackageAssistantApp/MainWindow/WpfApp1/App.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+using System.Windows.Media;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    internal static class LocalExtensions
+    {
+        public static void ForWindowFromTemplate(this object templateFrameworkElement, Action<Window> action)
+        {
+            Window window = ((FrameworkElement)templateFrameworkElement).TemplatedParent as Window;
+            if (window != null) action(window);
+        }
+
+        public static IntPtr GetWindowHandle(this Window window)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(window);
+            return helper.Handle;
+        }
+    }
+    public partial class App : Application
+    {
+        void IconMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount > 1)
+                sender.ForWindowFromTemplate(w => SystemCommands.CloseWindow(w));
+        }
+
+        void IconMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            var element = sender as FrameworkElement;
+            var point = element.PointToScreen(new Point(element.ActualWidth / 2, element.ActualHeight));
+            sender.ForWindowFromTemplate(w => SystemCommands.ShowSystemMenu(w, point));
+        }
+
+        void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            ((Window)sender).StateChanged += WindowStateChanged;
+        }
+
+        void WindowStateChanged(object sender, EventArgs e)
+        {
+            var w = ((Window)sender);
+            var handle = w.GetWindowHandle();
+            var containerBorder = (Border)w.Template.FindName("PART_Container", w);
+
+            if (w.WindowState == WindowState.Maximized)
+            {
+                // Make sure window doesn't overlap with the taskbar.
+                var screen = System.Windows.Forms.Screen.FromHandle(handle);
+                if (screen.Primary)
+                {
+                    containerBorder.Padding = new Thickness(
+                        SystemParameters.WorkArea.Left + 7,
+                        SystemParameters.WorkArea.Top + 7,
+                        (SystemParameters.PrimaryScreenWidth - SystemParameters.WorkArea.Right) + 7,
+                        (SystemParameters.PrimaryScreenHeight - SystemParameters.WorkArea.Bottom) + 5);
+                }
+            }
+            else
+            {
+                containerBorder.Padding = new Thickness(7, 7, 7, 5);
+            }
+        }
+
+        void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            sender.ForWindowFromTemplate(w => SystemCommands.CloseWindow(w));
+        }
+
+        void MinButton_Click(object sender, RoutedEventArgs e)
+        {
+            sender.ForWindowFromTemplate(w => SystemCommands.MinimizeWindow(w));
+        }
+
+        void MaxButton_Click(object sender, RoutedEventArgs e)
+        {
+            sender.ForWindowFromTemplate(w =>
+            {
+                if (w.WindowState == WindowState.Maximized) SystemCommands.RestoreWindow(w);
+                else SystemCommands.MaximizeWindow(w);
+            });
+        }
+
+        private void GramophoneButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
